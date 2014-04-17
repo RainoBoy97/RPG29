10a2883a24532e1c4ae404a7026985411e5b3687
RainoBoy97 <rainoboy97@gmail.com>
2014-04-17 16:17:42 +0200

    Make ResourceUtil easier to use

--- src/me/pogostick29dev/rpg29/util/ResourceUtil.java
+++ src/me/pogostick29dev/rpg29/util/ResourceUtil.java
@@ -10,27 +10,21 @@ public class ResourceUtil {
 
     private ResourceUtil() { }
 
-    private static final ResourceUtil instance = new ResourceUtil();
+    private static Class<?> clazz;
 
-    public static ResourceUtil getInstance() {
-        return instance;
-    }
-
-    private Class<?> clazz;
-
-    public void setup(Class<?> clazz) {
+    public static void setup(Class<?> clazz) {
         this.clazz = clazz;
     }
 
-    public URL getResource(String path) {
+    public static URL getResource(String path) {
         return clazz.getResource("/res/" + path);
     }
 
-    public InputStream getResourceAsStream(String path) {
+    public static InputStream getResourceAsStream(String path) {
         return clazz.getResourceAsStream("/res/" + path);
     }
 
-    public BufferedImage getBufferedImage(String path) {
+    public static BufferedImage getBufferedImage(String path) {
         try {
             return ImageIO.read(getResource(path + ".png"));
         } catch (Exception e) {
@@ -38,15 +32,15 @@ public class ResourceUtil {
         }
     }
 
-    public ImageIcon getImage(String path) {
+    public static ImageIcon getImage(String path) {
         return new ImageIcon(getBufferedImage(path));
     }
 
-    public ImageIcon getImage(String path, int width, int height) {
+    public static ImageIcon getImage(String path, int width, int height) {
         return resizeImage(getImage(path), width, height);
     }
 
-    public ImageIcon resizeImage(ImageIcon image, int width, int height) {
+    public static ImageIcon resizeImage(ImageIcon image, int width, int height) {
         return new ImageIcon(image.getImage().getScaledInstance(width, height, 0));
     }
 }
